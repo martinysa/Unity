2393d138d40f2f3d1b5ba50616eed5b5c8e5774d
martinysa <martin@MacBook-Pro-de-Martin.local>
2013-10-04 19:45:42 -0300

    cameraTouch update

--- Chapter1/MoverClass/Assets/cameraTouchBehavior.js
+++ Chapter1/MoverClass/Assets/cameraTouchBehavior.js
@@ -3,22 +3,26 @@
 	
 	var guiLog: GUIText;
 	// The ID of the touch that began the scroll.
-   var ScrollTouchID: int = -1;
-   // The position of that initial touch
-   var ScrollTouchOrigin: Vector2;
+	
+	var ScrollTouchID: int = -1;
+	// The position of that initial touch
+	
+	//var ScrollTouchOrigin: Vector2;
+	
+	private var startTouchPosition : Vector2;
 
+	
+	
 	private var isTouchDevice : boolean = false;
+	
+	
+	
 
 	function Awake() {
 		if (Application.platform == RuntimePlatform.IPhonePlayer) 
 			isTouchDevice = true; 
 		else
 			isTouchDevice = false; 
-
-    	if (Application.platform == RuntimePlatform.IPhonePlayer) 
-        	isTouchDevice = true; 
-    	else
-        	isTouchDevice = false; 
 
 	}
 	
@@ -29,32 +33,54 @@
 
 	}
 
-	function Update () {
+	function LateUpdate () {
 	
-		var myTouches:Touch[] = Input.touches;
+		//var myTouches:Touch[];
 		
-    	for(var i:int=0;i<Input.touchCount;i++)
-    	{
-        	Debug.Log(myTouches[i].position);
-        	Debug.Log(i);
-        	guiLog.text = myTouches[i].position.x + " : " + myTouches[i].position.y;
-    	}	
+    
+		handleCamera();
 	}
 
 
 
 function handleCamera(){
-	var clickDetected : boolean;
-    var touchPosition : Vector3;
- 
-    // Detect click and calculate touch position
-    if (isTouchDevice) {
-        clickDetected = (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began);
-        touchPosition = Input.GetTouch(0).position;
-    } else {
-        clickDetected = (Input.GetMouseButtonDown(0));
-        touchPosition = Input.mousePosition;
-    }
+   var sensivility: float = .07;
+		
+		if (isTouchDevice) { // iphone screen
+			var clickDetected: boolean = (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began);
+	        
+	        if(clickDetected){
+	        	startTouchPosition = Input.GetTouch(0).position;
+    		}else{
+    			var currentTouchPosition: Vector2 = Input.GetTouch(0).position;
+    			var finalCameraPosition: Vector2 = (startTouchPosition - currentTouchPosition) * sensivility;
+    			startTouchPosition = currentTouchPosition;
+    			Camera.main.transform.position += new Vector3(finalCameraPosition.x, finalCameraPosition.y, 0);
+    		}
+    		
+    		//Restore startPosition
+    		if(Input.GetTouch(0).phase == TouchPhase.Ended)
+    			startTouchPosition *= 0;
+    	
+    	} else { // mouse 
+	        clickDetected = (Input.GetMouseButtonDown(0));
+	       if(clickDetected && startTouchPosition.x == 0){
+	        	startTouchPosition = Input.mousePosition;
+	    	}else if(!clickDetected && startTouchPosition.x != 0 && Input.GetAxis("Mouse X") != 0){
+    			currentTouchPosition = Input.mousePosition;
+    			finalCameraPosition = (startTouchPosition - currentTouchPosition) * sensivility;
+    			Camera.main.transform.position += new Vector3(finalCameraPosition.x, finalCameraPosition.y, 0);
+    			startTouchPosition = currentTouchPosition;
+    			guiLog.text = finalCameraPosition.x + " : " + finalCameraPosition.y + " : " + Camera.main.transform.position.z;
+    		}
+	       
+	       //Restore startPosition
+	       if(Input.GetMouseButtonUp(0))
+	       		startTouchPosition *= 0;
+	       
+	    }
+		
+	
     
 
 }
